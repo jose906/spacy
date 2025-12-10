73c754f3e24905b4add73e2f4de1e73c170dd126
jose906 <jrestenssoro@hotmail.com>
2025-12-10 13:31:28 -0400

    spacy

--- spacyscript.py
+++ spacyscript.py
@@ -1,12 +1,14 @@
 import spacy
-
-
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import re
 
 nlp = spacy.load("es_core_news_lg")
 
 # Configura tu conexión MySQL
 # --- 2. Función para extraer entidades ---
 def get_entities(text):
+    text = preprocess_text(text)
     if not text or not isinstance(text, str):
         return {"PER": [], "ORG": [], "LOC": [], "MISC": []}
 
@@ -21,4 +23,21 @@ def get_entities(text):
     return entidades
 
 
+def preprocess_text(text: str) -> str:
+    lemmatizer = WordNetLemmatizer()
+    # Quitar caracteres no alfabéticos
+    r = re.sub('[^a-zA-Z]', ' ', text)
+    r = r.lower()
+    r = r.split()
+
+    # Quitar stopwords
+    r = [word for word in r if word not in stopwords.words('english')]
+
+    # Lemmatizar
+    r = [lemmatizer.lemmatize(word) for word in r]
+
+    # Volver a string
+    return ' '.join(r)
+    
+    
 
